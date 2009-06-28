875b37900510ccf1ae4cfe7ee1d128f470541b9a
octavio@servicios-corporativos.com <octavio@servicios-corporativos.com@a36d047a-ba86-11dd-b55b-2da8d274f894>
2009-06-28 17:45:33 +0000

    git-svn-id: http://openomoikane2.googlecode.com/svn/trunk/Omoikane@130 a36d047a-ba86-11dd-b55b-2da8d274f894

---
+++ test/omoikane/sistema/n2tTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package omoikane.sistema;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author SYSTEM
+ */
+public class n2tTest {
+
+    public n2tTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    @Test
+    public void convertirLetrasTest() {
+        n2t n = new n2t();
+        System.out.println(n.convertirLetras(1500));
+        System.out.println(n.convertirLetras(11753));
+        System.out.println(n.convertirLetras(100));
+        System.out.println(n.convertirLetras(13296));
+
+        System.out.println(n.aCifra(48223.87));
+    }
+
+}
