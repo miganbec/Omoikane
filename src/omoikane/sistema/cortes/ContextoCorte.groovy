c908178ca7d9733ecd7d733392bbf8568e829c2f
octavio@servicios-corporativos.com <octavio@servicios-corporativos.com@a36d047a-ba86-11dd-b55b-2da8d274f894>
2009-07-09 20:03:45 +0000

    ContextoCorte.groovy
    git-svn-id: http://openomoikane2.googlecode.com/svn/trunk/Omoikane@132 a36d047a-ba86-11dd-b55b-2da8d274f894

---
+++ src/omoikane/sistema/cortes/ContextoCorte.groovy
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package omoikane.sistema.cortes
+
+/**
+ *
+ * @author SYSTEM
+ */
+class ContextoCorte {
+	
+}
+
